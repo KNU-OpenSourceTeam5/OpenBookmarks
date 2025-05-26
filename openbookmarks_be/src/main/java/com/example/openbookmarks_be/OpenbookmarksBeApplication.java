8806925b1f29447d7493fb0fae7b72a4bb4aeb1b
thdgmlwns1 <xhrl233@naver.com>
2025-05-26 10:59:33 +0900

    Add: be_Repo

---
+++ openbookmarks_be/src/main/java/com/example/openbookmarks_be/OpenbookmarksBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.openbookmarks_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbookmarksBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenbookmarksBeApplication.class, args);
+	}
+
+}
---
+++ openbookmarks_be/src/test/java/com/example/openbookmarks_be/OpenbookmarksBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openbookmarks_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbookmarksBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
